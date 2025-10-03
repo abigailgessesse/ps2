a2bb157a910ebb04929096b82cb78ebc9e5a67f3
Abby Gessesse <abbygessesse@MacBook-Air-329.local>
2025-10-02 21:05:35 -0700

    pset2

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import random
 # clean_input
 # validate_input
 # check_winner
-# TODO: YOUR NAME HERE: __________________________
+# YOUR NAME HERE: Abigail Gessesse
 #_______________________________________________________________________________________________________________________
 
 def get_user_choice():
@@ -21,13 +21,12 @@ def get_user_choice():
     Ask the user for their move and save it! 
     Problem 1: 10 points possible
     """
-    # TODO: Prompt the user to enter their choice.
-    user_choice = None # Change None to be the user's input!
+    user_choice = input("Enter your choice: ")
 
     return user_choice  # Return the user's choice (No need to modify this line)
 
-# You can test your function by running pytest -k test_get_user_choice in the terminal!
-# This test is a practice to get you started, so make sure it passes before you continue!
+# pytest -k test_get_user_choice
+
 
 
 #_______________________________________________________________________________________________________________________
@@ -37,23 +36,19 @@ def get_computer_choice():
      Generates a random choice for the computer.
      Problem 2: 15 points possible
      """
-     
-     # TODO: Generate a random number between 1 and 3 using the 'random' module.
-     # Call this variable 'computer_choice'. This will be the computer's choice.
-     # HINT: You might find this link to be helpful! https://www.w3schools.com/python/ref_random_randint.asp
-     
-     computer_choice = None  # Change None to be your random number!
-     
-     # TODO: Convert the number into rock (1), paper (2), or scissors (3) using if statements.
-     # Set the computer_choice variable to the string value of the choice. 
-     # HINT: Make sure to use the exact strings "rock", "paper", or "scissors".
+     # ANOTHER OPTION: computer_choice = choices[random.randint(0, 2)]
+     # choices = ["rock", "paper", "scissors"]
+     number = random.randint(1, 3)
+     if number == 1:
+        computer_choice = "rock"
+     elif number == 2:
+        computer_choice = "paper"
+     else:  # number == 3
+        computer_choice = "scissors"
+ 
+     return computer_choice
 
-     
-     
-     return computer_choice  # Return the computer's choice (No need to modify this line)
-
-# You can test your function by running pytest -k test_get_computer_choice in the terminal!
-# After each function you MUST make sure ALL your tests pass before you continue!
+# pytest -k test_get_computer_choice
 
 #_______________________________________________________________________________________________________________________
 
@@ -63,12 +58,10 @@ def clean_input(user_choice):
      Use the string methods we have learned in class to do this!
      Problem 3: 20 points possible
      """
-     
-     # TODO: Set user_choice to be a lowercase version of itself
-     
-     # TODO: Set user_choice to be itself, but with leading/trailing whitespace removed
+     user_choice = user_choice.lower()
+     user_choice = user_choice.strip()
 
-     return None  # Return the cleaned user choice
+     return user_choice
 
 # You can test your function by running pytest -k test_clean_input in the terminal!
 
@@ -84,19 +77,21 @@ def validate_input(user_choice):
      - 
      Problem 4: 15 points possible
     """ 
-    
-    # TODO: Check if the user's input is a string, and set the variable to True if it is and False if not.
-    # Clean the user's input using your clean_input function ONLY if the input is a string.
-    is_string = None
 
-    
-    # TODO: Check if the user's input is either "rock", "paper", or "scissors", and set the variable to a boolean value.
-    is_valid_choice = None
-    
+    if type(user_choice) == str:
+        is_string = True
+        user_choice = clean_input(user_choice)
+    else:
+        is_string = False
 
-    
-    # TODO: If the user's input is a string AND a valid choice, RETURN True. Otherwise, RETURN False.
-    return None  # Return the result
+    is_valid_choice = False
+    if user_choice == "rock" or user_choice == "paper" or user_choice == "scissors":
+        is_valid_choice = True
+
+    if is_string and is_valid_choice:
+        return True
+    else:
+        return False
 
 # You can test your function by running pytest -k test_validate_input in the terminal!
 
@@ -105,35 +100,33 @@ def validate_input(user_choice):
 def check_winner(user_choice, computer_choice):
      """
      Determines the winner based on user and computer choices.
-     
-     Don't worry about handling user errors here, we will have already checked for those.
+
+
      Problem 5: 25 points possible
      """
      
-     result = None # Initialize the result variable
+     result = " "
+     is_input_valid = validate_input(user_choice)
+     if is_input_valid == False:
+         return "Invalid Input Try Again Next Time!"
 
-     # YOUR CODE HERE
-     is_input_valid = validate_input(user_choice)  # No need to modify this line
-     # TODO: if the user input is not valid, result should be "Invalid Input Try Again Next Time!."
-  
-     
-     # TODO: Determine the winner by using if statements.
-     
-     # Here is an example of how to compare the user and computer choices
-     if user_choice == "rock" and computer_choice == "scissors":
-         result = "You win!"
-         
-     # TODO: If the user wins, your result should be "You win!"
-     
-     # TODO: If the computer wins, your result should be "Better luck next time!"
-     
-     # TODO: If the user and computer choices are the same, your result should be "It's a tie!"
+     # User and Computer Tie
+     if user_choice == computer_choice:
+        result = "It's a tie!"
+     # User win cases
+     elif user_choice == "rock" and computer_choice == "scissors":
+        result = "You win!"
+     elif user_choice == "paper" and computer_choice == "rock":
+        result = "You win!"
+     elif user_choice == "scissors" and computer_choice == "paper":
+        result = "You win!"
+     # Computer win cases
+     else:
+        result = "Better luck next time!"
     
-     return result  # Return the result
+     return result
 
-# You can test your function by running pytest -k test_check_winner in the terminal!
-
-# Now that you have completed all the functions, we can play the game! Run the main.py file to play the game!
+# pytest -k test_check_winner
 
 #_______________________________________________________________________________________________________________________
 
@@ -145,7 +138,6 @@ def start_game():
      print(check_winner(user_choice, computer_choice))  # Determine the winner
      return
 
-# Run uncomment the line below this and run python3 main.py in the terminal to play the game!
+
 # start_game() 
 
-# BE SURE TO COMMENT THE LINE ABOVE OUT BEFORE RUNNING YOUR TESTS AND SUBMITTING YOUR CODE!
